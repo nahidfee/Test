e108a06b7367a4849bf91bcdce7f66a8a77575e5
Nahid Hasan <88654815+nahidfee@users.noreply.github.com>
2024-10-12 22:10:46 +0600

    mineclaimer.py

--- bot/mineclaimer.py
+++ bot/mineclaimer.py
@@ -6,7 +6,6 @@ from bot.utils import night_sleep, Colors
 async def mine_claimer(NotPxClient, session_name):
     await asyncio.sleep(5)  # Start with a delay...
     print("[+] {}Auto claiming started{}.".format(Colors.CYAN, Colors.END))
-    
     while True:
         await night_sleep()  # Check and sleep if it's between 12-1 AM Iran time
         acc_data = NotPxClient.accountStatus()
@@ -20,7 +19,7 @@ async def mine_claimer(NotPxClient, session_name):
             fromStart = acc_data['fromStart']
             speedPerSecond = acc_data['speedPerSecond']
             maxMiningTime = acc_data['maxMiningTime'] / 60
-            random_recharge_speed = random.randint(30, 90)
+            random_recharge_speed = random.randint(30,90)
 
             if fromStart * speedPerSecond > 0.3:
                 claimed_count = round(NotPxClient.claim_mining(), 2)
@@ -31,8 +30,5 @@ async def mine_claimer(NotPxClient, session_name):
         else:
             print("[!] {}{}{}: {}Unexpected account data format. Retrying...{}".format(Colors.CYAN, session_name, Colors.END, Colors.RED, Colors.END))
         
-        print("[!] {}{}{}: Sleeping for {} minutes...".format(
-            Colors.CYAN, session_name, Colors.END,
-            round((maxMiningTime + random_recharge_speed) / 60), 2
-        ))
-        await asyncio.sleep(maxMiningTime + random_recharge_speed)
+        print("[!] {}{}{}: Sleeping for {} minutes...".format(Colors.CYAN, session_name, Colors.END,round((maxMiningTime+random_recharge_speed)/60),2))
+        await asyncio.sleep(maxMiningTime+random_recharge_speed)
